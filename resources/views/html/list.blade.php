b0b9ff4110cbdcb3c1c77e3b57a9b216f42b3b82
Christopher Mühl <christopher@padarom.io>
2018-04-30 09:58:52 +0200

    Use Bulma for a prettier frontend

--- resources/views/html/list.blade.php
+++ resources/views/html/list.blade.php
@@ -4,57 +4,82 @@
         <meta charset="utf-8">
         <title>{{ $pageTitle }}</title>
 
-        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.8.0/css/flag-icon.min.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flag-icon-css/2.8.0/css/flag-icon.min.css">
 
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
     </head>
     <body>
-        <nav class="navbar navbar-default navbar-static-top">
+        <nav class="navbar is-danger">
             <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">{{ $pageTitle }}</a>
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="/">{{ $pageTitle }}</a>
                 </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#login">Login</a></li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Language <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
+
+                <div id="navMenuExample" class="navbar-menu">
+                    <div class="navbar-end">
+                        <a class="navbar-item" href="#">
+                            Login
+                        </a>
+
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a href="#" class="navbar-link">
+                                Language
+                            </a>
+                            <div class="navbar-dropdown">
                                 @foreach($languages as $language)
                                     @if ($language == 'en')
-                                        <li><a href="#"><span class="flag-icon flag-icon-gb"></span> en</a></li>
+                                        <a class="navbar-item" href="#"><span class="flag-icon flag-icon-gb"></span> en</a>
                                     @else
-                                        <li><a href="#"><span class="flag-icon flag-icon-{{ $language }}"></span> {{ $language }}</a></li>
+                                        <a class="navbar-item" href="#"><span class="flag-icon flag-icon-{{ $language }}"></span> {{ $language }}</a>
                                     @endif
                                 @endforeach
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!--/.nav-collapse -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
 
         <div class="container">
-            <table class="table table-hover">
+            <div class="packages">
                 @foreach ($packages as $package)
-                    <tr>
-                        <td>{{ $package->identifier }}</td>
-                    </tr>
+                    <div class="box">
+                        <div class="level">
+                            <div class="level-left"><strong>{{ $package->name }}</strong></div>
+                            <div class="level-right">
+                                <a href="/package/{{ $package->identifier }}"><div class="tag">{{ $package->identifier }}</div></a>
+                            </div>
+                        </div>
+                        {{ $package->description }}<br>
+                        <span class="is-size-7">
+                            @if ($package->authorurl)
+                                By <a href="{{ $package->authorurl }}">{{ $package->author }}</a>
+                            @else 
+                                By {{ $package->author }}
+                            @endif
+                        </span>
+                    </div>
                 @endforeach
-            </table>
-            <footer class="text-right text-muted">
-                <small>
-                    Built with <a href="https://github.com/padarom/thunderstorm">Padarom's Thunderstorm</a> -- Click <a href="{!! $query !!}">here</a> for the XML version.
-                </small>
-            </footer>
+            </div>
+
+            <div class="level">
+                <div class="level-left"></div>
+                <div class="level-right">
+                    <div class="level-item">
+                        <small class="is-size-7">
+                            Built with <a href="https://github.com/padarom/thunderstorm">Padarom's Thunderstorm</a> -- Click <a href="{!! $query !!}">here</a> for the XML version.
+                        </small>
+                    </div>
+                </div>
+            </div>
         </div>
+
+        {{-- Inlined style for ease of deployment and development --}}
+        <style>
+            .packages { margin: 30px 0; }
+            .flag-icon { margin-right: 6px; }
+            .level { margin-bottom: 0 !important;}
+        </style>
     </body>
 </html>
